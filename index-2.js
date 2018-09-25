059eb047d8036b1f6f57d8928653cd4b052fe131
Techmonster20 <aidan@aidanolson.com>
2018-09-25 17:17:57 -0500

    Token Fix

--- index-2.js
+++ index-2.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fs = require('fs');
 const APP = require('./package.json');
-const token = 'bot.token';
+const token = 'BOT_TOKEN';
 
 client.on('ready', () => {
   console.log('I am ready!')
@@ -248,4 +248,4 @@ async function handleVideo(video, message, voiceChannel, playlist = false) {
 	serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
 }
 });
-client.login('bot.token');
+client.login('BOT_TOKEN');
